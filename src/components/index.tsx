08a38f1952dddfdc30d69f472e72f07472985c3e
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-21 12:13:11 +0200

    first commit

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+export { Navbar } from "./Navbar/Navbar";
+export { Hello } from "./Hello/Hello";
+export { About } from "./About/About";
+export { Projects } from "./Projects/Projects";
+export { Footer } from "./Footer/Footer";

1e3853a2e48c45b140ade3a8a950af2e14831d32
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-22 19:00:25 +0200

    feat: creating navbar layout

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,17 @@
+import { Link } from "react-router-dom";
+
 export function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="nav">
+      <Link className="nav__link" to="/">
+        _hello
+      </Link>
+      <Link className="nav__link" to="/about">
+        _about
+      </Link>
+      <Link className="nav__link" to="/projects">
+        _projects
+      </Link>
+    </nav>
+  );
 }

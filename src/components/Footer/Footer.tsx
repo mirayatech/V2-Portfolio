52868243c3160989f70a8ad17809a80b2ff62140
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-23 07:06:55 +0200

    feat(footer): add social links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import { FiGithub, FiLinkedin, FiInstagram } from "react-icons/fi";
 export function Footer() {
   const current = new Date();
   const date = `${current.getDate()}/${
@@ -8,7 +9,21 @@ export function Footer() {
 
   return (
     <footer className="footer">
-      <aside className="footer__socials"></aside>
+      <aside className="footer__socials">
+        <p>find me in:</p>
+        <a
+          href="https://www.linkedin.com/in/miraya-abrodi-a48382237/"
+          target="_blank"
+        >
+          <FiLinkedin />
+        </a>
+        <a href="https://github.com/mirayatech" target="_blank">
+          <FiGithub />
+        </a>
+        <a href="https://www.instagram.com/mirayatech/" target="_blank">
+          <FiInstagram />
+        </a>
+      </aside>
       <aside className="footer__time">
         <p> {time}</p>
         <p>{date}</p>

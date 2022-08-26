b20a6a971ce58ec658b63420962f00075e9d0441
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-26 19:48:51 +0200

    style: make project responsive

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -3,27 +3,29 @@ import { motion } from "framer-motion";
 
 export function Projects() {
   return (
-    <div className="projects">
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: 0.2,
+    <>
+      {" "}
+      <h1 className="projects__title">//Top Projects</h1>
+      <div className="projects">
+        <motion.a
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
             },
-          },
-        }}
-        className="first__project"
-      >
-        <h1 className="projects__title">//Top Projects</h1>
-        <a href="https://github.com/mirayatech/E-Commerce" target="_blank">
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 0.2,
+              },
+            },
+          }}
+          href="https://github.com/mirayatech/E-Commerce"
+          target="_blank"
+        >
           <div className="project">
             <div className="title">
               <h2>E-Commerce</h2>
@@ -47,300 +49,310 @@ export function Projects() {
               React&nbsp;&nbsp;&nbsp;TypeScript&nbsp;&nbsp;&nbsp;CSS
             </div>
           </div>
-        </a>
-      </motion.div>
-      <motion.a
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: 0.4,
-            },
-          },
-        }}
-        href="https://github.com/mirayatech/Netflix-Clone"
-        target="_blank"
-      >
-        <div className="project">
-          <div className="title">
-            <h2>Netflix Clone</h2>
-            <div className="title__wrapper">
-              <a
-                href="https://github.com/mirayatech/Netflix-Clone"
-                target="_blank"
-              >
-                <FiGithub />
-              </a>
-              <a href="https://netflix-mirayatech.vercel.app/" target="_blank">
-                <FiExternalLink />
-              </a>
-            </div>
-          </div>
-          <p>
-            Netflix clone build with react and typescript. User are able to mute
-            and unmute the trailer video.
-          </p>
-          <div className="language">
-            React&nbsp;&nbsp;&nbsp;TypeScript&nbsp;&nbsp;&nbsp;API&nbsp;&nbsp;&nbsp;CSS
-          </div>
-        </div>
-      </motion.a>
+        </motion.a>
 
-      <motion.a
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: 0.6,
+        <motion.a
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
             },
-          },
-        }}
-        href="https://github.com/mirayatech/Note-App"
-        target="_blank"
-      >
-        <div className="project">
-          <div className="title">
-            <h2>Note App</h2>
-            <div className="title__wrapper">
-              <a href="https://github.com/mirayatech/Note-App" target="_blank">
-                <FiGithub />
-              </a>
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 0.4,
+              },
+            },
+          }}
+          href="https://github.com/mirayatech/Netflix-Clone"
+          target="_blank"
+        >
+          <div className="project">
+            <div className="title">
+              <h2>Netflix Clone</h2>
+              <div className="title__wrapper">
+                <a
+                  href="https://github.com/mirayatech/Netflix-Clone"
+                  target="_blank"
+                >
+                  <FiGithub />
+                </a>
+                <a
+                  href="https://netflix-mirayatech.vercel.app/"
+                  target="_blank"
+                >
+                  <FiExternalLink />
+                </a>
+              </div>
+            </div>
+            <p>
+              Netflix clone build with react and typescript. User are able to
+              mute and unmute the trailer video.
+            </p>
+            <div className="language">
+              React&nbsp;&nbsp;&nbsp;TypeScript&nbsp;&nbsp;&nbsp;API&nbsp;&nbsp;&nbsp;CSS
             </div>
           </div>
-          <p>
-            An app where you can take notes. User can use markdown language in
-            the note app.
-          </p>
-          <div className="language">
-            React&nbsp;&nbsp;&nbsp;CSS&nbsp;&nbsp;&nbsp;VS-Code
-          </div>
-        </div>
-      </motion.a>
+        </motion.a>
 
-      <motion.a
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: 0.8,
+        <motion.a
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
             },
-          },
-        }}
-        href="https://github.com/mirayatech/Github-Finder"
-        target="_blank"
-      >
-        <div className="project">
-          <div className="title">
-            <h2>Github Finder</h2>
-            <div className="title__wrapper">
-              <a
-                href="https://github.com/mirayatech/Github-Finder"
-                target="_blank"
-              >
-                <FiGithub />
-              </a>
-              <a href="https://ghfinder-phi.vercel.app/" target="_blank">
-                <FiExternalLink />
-              </a>
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 0.6,
+              },
+            },
+          }}
+          href="https://github.com/mirayatech/Note-App"
+          target="_blank"
+        >
+          <div className="project">
+            <div className="title">
+              <h2>Note App</h2>
+              <div className="title__wrapper">
+                <a
+                  href="https://github.com/mirayatech/Note-App"
+                  target="_blank"
+                >
+                  <FiGithub />
+                </a>
+              </div>
+            </div>
+            <p>
+              An app where you can take notes. User can use markdown language in
+              the note app.
+            </p>
+            <div className="language">
+              React&nbsp;&nbsp;&nbsp;CSS&nbsp;&nbsp;&nbsp;VS-Code
             </div>
           </div>
-          <p>
-            GitHub Finder makes it super easy to find any random GitHub user and
-            see their work.
-          </p>
-          <div className="language">
-            JavaScript&nbsp;&nbsp;&nbsp;HTML&nbsp;&nbsp;&nbsp;CSS
-          </div>
-        </div>
-      </motion.a>
+        </motion.a>
 
-      <motion.a
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: 1,
+        <motion.a
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
             },
-          },
-        }}
-        href="https://github.com/mirayatech/Movie-App"
-        target="_blank"
-      >
-        <div className="project">
-          <div className="title">
-            <h2>Movie App</h2>
-            <div className="title__wrapper">
-              <a href="https://github.com/mirayatech/Movie-App" target="_blank">
-                <FiGithub />
-              </a>
-              <a href="https://movieflix-navy.vercel.app/" target="_blank">
-                <FiExternalLink />
-              </a>
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 0.8,
+              },
+            },
+          }}
+          href="https://github.com/mirayatech/Github-Finder"
+          target="_blank"
+        >
+          <div className="project">
+            <div className="title">
+              <h2>Github Finder</h2>
+              <div className="title__wrapper">
+                <a
+                  href="https://github.com/mirayatech/Github-Finder"
+                  target="_blank"
+                >
+                  <FiGithub />
+                </a>
+                <a href="https://ghfinder-phi.vercel.app/" target="_blank">
+                  <FiExternalLink />
+                </a>
+              </div>
+            </div>
+            <p>
+              GitHub Finder makes it super easy to find any random GitHub user
+              and see their work.
+            </p>
+            <div className="language">
+              JavaScript&nbsp;&nbsp;&nbsp;HTML&nbsp;&nbsp;&nbsp;CSS
             </div>
           </div>
-          <p>
-            A way to search for movies and see their rating , release date and
-            summary.
-          </p>
-          <div className="language">
-            JavaScript&nbsp;&nbsp;&nbsp;HTML&nbsp;&nbsp;&nbsp;CSS
-          </div>
-        </div>
-      </motion.a>
+        </motion.a>
 
-      <motion.a
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: 1.2,
+        <motion.a
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
             },
-          },
-        }}
-        href="https://github.com/mirayatech/Anime-Quote"
-        target="_blank"
-      >
-        <div className="project">
-          <div className="title">
-            <h2>Anime Quote</h2>
-            <div className="title__wrapper">
-              <a
-                href="https://github.com/mirayatech/Anime-Quote"
-                target="_blank"
-              >
-                <FiGithub />
-              </a>
-              <a href="https://anime-quotes-rho.vercel.app/" target="_blank">
-                <FiExternalLink />
-              </a>
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 1,
+              },
+            },
+          }}
+          href="https://github.com/mirayatech/Movie-App"
+          target="_blank"
+        >
+          <div className="project">
+            <div className="title">
+              <h2>Movie App</h2>
+              <div className="title__wrapper">
+                <a
+                  href="https://github.com/mirayatech/Movie-App"
+                  target="_blank"
+                >
+                  <FiGithub />
+                </a>
+                <a href="https://movieflix-navy.vercel.app/" target="_blank">
+                  <FiExternalLink />
+                </a>
+              </div>
+            </div>
+            <p>
+              A way to search for movies and see their rating , release date and
+              summary.
+            </p>
+            <div className="language">
+              JavaScript&nbsp;&nbsp;&nbsp;HTML&nbsp;&nbsp;&nbsp;CSS
             </div>
           </div>
-          <p>
-            Random Amine Quote Generator used to generate random quotes. Each
-            time you click on the button, you’ll get a new anime quote.
-          </p>
-          <div className="language">
-            JavaScript&nbsp;&nbsp;&nbsp;HTML&nbsp;&nbsp;&nbsp;CSS
-          </div>
-        </div>
-      </motion.a>
+        </motion.a>
 
-      <motion.a
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: 1.4,
+        <motion.a
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
             },
-          },
-        }}
-        href="https://github.com/mirayatech/Tic-Tac-Toe"
-        target="_blank"
-      >
-        <div className="project">
-          <div className="title">
-            <h2>Tic Tac Toe</h2>
-            <div className="title__wrapper">
-              <a
-                href="https://github.com/mirayatech/Tic-Tac-Toe"
-                target="_blank"
-              >
-                <FiGithub />
-              </a>
-              <a href="https://toxo-two.vercel.app/" target="_blank">
-                <FiExternalLink />
-              </a>
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 1.2,
+              },
+            },
+          }}
+          href="https://github.com/mirayatech/Anime-Quote"
+          target="_blank"
+        >
+          <div className="project">
+            <div className="title">
+              <h2>Anime Quote</h2>
+              <div className="title__wrapper">
+                <a
+                  href="https://github.com/mirayatech/Anime-Quote"
+                  target="_blank"
+                >
+                  <FiGithub />
+                </a>
+                <a href="https://anime-quotes-rho.vercel.app/" target="_blank">
+                  <FiExternalLink />
+                </a>
+              </div>
+            </div>
+            <p>
+              Random Amine Quote Generator used to generate random quotes. Each
+              time you click on the button, you’ll get a new anime quote.
+            </p>
+            <div className="language">
+              JavaScript&nbsp;&nbsp;&nbsp;HTML&nbsp;&nbsp;&nbsp;CSS
             </div>
           </div>
-          <p>
-            A Tic Tac Toe Game built with pure JavaScript. User plays against
-            the computer.
-          </p>
-          <div className="language">
-            JavaScript&nbsp;&nbsp;&nbsp;HTML&nbsp;&nbsp;&nbsp;CSS
-          </div>
-        </div>
-      </motion.a>
+        </motion.a>
 
-      <motion.a
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: 1.6,
+        <motion.a
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
             },
-          },
-        }}
-        href="https://github.com/mirayatech/Mitsuki"
-        target="_blank"
-      >
-        <div className="project">
-          <div className="title">
-            <h2>Mitsuki</h2>
-            <div className="title__wrapper">
-              <a href="https://github.com/mirayatech/Mitsuki" target="_blank">
-                <FiGithub />
-              </a>
-              <a href="https://mitsuki-psi.vercel.app/" target="_blank">
-                <FiExternalLink />
-              </a>
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 1.4,
+              },
+            },
+          }}
+          href="https://github.com/mirayatech/Tic-Tac-Toe"
+          target="_blank"
+        >
+          <div className="project">
+            <div className="title">
+              <h2>Tic Tac Toe</h2>
+              <div className="title__wrapper">
+                <a
+                  href="https://github.com/mirayatech/Tic-Tac-Toe"
+                  target="_blank"
+                >
+                  <FiGithub />
+                </a>
+                <a href="https://toxo-two.vercel.app/" target="_blank">
+                  <FiExternalLink />
+                </a>
+              </div>
+            </div>
+            <p>
+              A Tic Tac Toe Game built with pure JavaScript. User plays against
+              the computer.
+            </p>
+            <div className="language">
+              JavaScript&nbsp;&nbsp;&nbsp;HTML&nbsp;&nbsp;&nbsp;CSS
             </div>
           </div>
-          <p>A way for people to manage their tasks and take some notes.</p>
-          <div className="language">
-            JavaScript&nbsp;&nbsp;&nbsp;HTML&nbsp;&nbsp;&nbsp;CSS
+        </motion.a>
+
+        <motion.a
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 1.6,
+              },
+            },
+          }}
+          href="https://github.com/mirayatech/Mitsuki"
+          target="_blank"
+        >
+          <div className="project">
+            <div className="title">
+              <h2>Mitsuki</h2>
+              <div className="title__wrapper">
+                <a href="https://github.com/mirayatech/Mitsuki" target="_blank">
+                  <FiGithub />
+                </a>
+                <a href="https://mitsuki-psi.vercel.app/" target="_blank">
+                  <FiExternalLink />
+                </a>
+              </div>
+            </div>
+            <p>A way for people to manage their tasks and take some notes.</p>
+            <div className="language">
+              JavaScript&nbsp;&nbsp;&nbsp;HTML&nbsp;&nbsp;&nbsp;CSS
+            </div>
           </div>
-        </div>
-      </motion.a>
-    </div>
+        </motion.a>
+      </div>
+    </>
   );
 }

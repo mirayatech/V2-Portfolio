7184516c3e6d2aaa28506def2f784fb71a542f62
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-26 14:09:00 +0200

    feat: make intrest and skills "folder" expandable

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,12 +1,18 @@
 import { motion } from "framer-motion";
 import { BsFillMarkdownFill } from "react-icons/bs";
-import { FiChevronDown } from "react-icons/fi";
+import { FiChevronDown, FiChevronRight } from "react-icons/fi";
 
 import { FaFolder } from "react-icons/fa";
 
 import "../../styles/About.css";
+import { useState } from "react";
 
 export function About() {
+  const [isOpenOne, setIsOpenOne] = useState(false);
+  const [isOpenTwo, setIsOpenTwo] = useState(false);
+  const [isOpenThree, setIsOpenThree] = useState(false);
+  const [isOpenFour, setIsOpenFour] = useState(false);
+
   return (
     <div className="about">
       <div className="about__primary">
@@ -185,48 +191,51 @@ export function About() {
       >
         <h2>Explorer</h2>
         <div className="wrapper-1">
-          <span className="arrow-down">
+          <span className="arrow">
             <FiChevronDown />
           </span>
           <p>Intrest & Skills</p>
         </div>
 
         <div className="wrapper-2">
-          <div className="title">
-            <span className="arrow-down">
-              <FiChevronDown />
+          <div onClick={() => setIsOpenOne(!isOpenOne)} className="title">
+            <span className="arrow">
+              {isOpenOne ? <FiChevronRight /> : <FiChevronDown />}
             </span>
             <span className="folder color-1">
               <FaFolder />
             </span>
             <p>Language</p>
           </div>
-
-          <div className="list">
-            {" "}
-            <p>
-              <BsFillMarkdownFill />
-              &nbsp; css.md
-            </p>
-            <p>
-              <BsFillMarkdownFill />
-              &nbsp; html.md
-            </p>
-            <p>
-              <BsFillMarkdownFill />
-              &nbsp; javascript.md
-            </p>
-            <p>
-              <BsFillMarkdownFill />
-              &nbsp; typescript.md
-            </p>
-          </div>
+          {isOpenOne ? (
+            <div className="list"></div>
+          ) : (
+            <div className="list">
+              {" "}
+              <p>
+                <BsFillMarkdownFill />
+                &nbsp; css.md
+              </p>
+              <p>
+                <BsFillMarkdownFill />
+                &nbsp; html.md
+              </p>
+              <p>
+                <BsFillMarkdownFill />
+                &nbsp; javascript.md
+              </p>
+              <p>
+                <BsFillMarkdownFill />
+                &nbsp; typescript.md
+              </p>
+            </div>
+          )}
         </div>
 
         <div className="wrapper-2">
-          <div className="title">
-            <span className="arrow-down">
-              <FiChevronDown />
+          <div onClick={() => setIsOpenTwo(!isOpenTwo)} className="title">
+            <span className="arrow">
+              {isOpenTwo ? <FiChevronDown /> : <FiChevronRight />}
             </span>
             <span className="folder color-2">
               <FaFolder />
@@ -234,78 +243,82 @@ export function About() {
             <p>Frameworks & Libraries</p>
           </div>
 
-          <div className="list">
-            <p>
-              <BsFillMarkdownFill />
-              &nbsp; react.md
-            </p>
-            <p>
-              <BsFillMarkdownFill />
-              &nbsp; sass.md
-            </p>{" "}
-            <p>
-              <BsFillMarkdownFill />
-              &nbsp; tailwind.md
-            </p>
-            <p>
-              <BsFillMarkdownFill />
-              &nbsp; material-ui.md
-            </p>
-            <p>
-              <BsFillMarkdownFill />
-              &nbsp; framer-motion.md
-            </p>
-          </div>
+          {isOpenTwo && (
+            <div className="list">
+              <p>
+                <BsFillMarkdownFill />
+                &nbsp; react.md
+              </p>
+              <p>
+                <BsFillMarkdownFill />
+                &nbsp; sass.md
+              </p>{" "}
+              <p>
+                <BsFillMarkdownFill />
+                &nbsp; tailwind.md
+              </p>
+              <p>
+                <BsFillMarkdownFill />
+                &nbsp; material-ui.md
+              </p>
+              <p>
+                <BsFillMarkdownFill />
+                &nbsp; framer-motion.md
+              </p>
+            </div>
+          )}
         </div>
 
         <div className="wrapper-2">
-          <div className="title">
-            <span className="arrow-down">
-              <FiChevronDown />
+          <div onClick={() => setIsOpenThree(!isOpenThree)} className="title">
+            <span className="arrow">
+              {isOpenThree ? <FiChevronDown /> : <FiChevronRight />}
             </span>
             <span className="folder color-3">
               <FaFolder />
             </span>
             <p>Extra</p>
           </div>
-
-          <div className=" list">
-            <p>
-              <BsFillMarkdownFill />
-              &nbsp; git.md
-            </p>
-            <p>
-              <BsFillMarkdownFill />
-              &nbsp; accessibility.md
-            </p>
-          </div>
+          {isOpenThree && (
+            <div className=" list">
+              <p>
+                <BsFillMarkdownFill />
+                &nbsp; git.md
+              </p>
+              <p>
+                <BsFillMarkdownFill />
+                &nbsp; accessibility.md
+              </p>
+            </div>
+          )}
         </div>
 
         <div className="wrapper-2">
-          <div className="title">
-            <span className="arrow-down">
-              <FiChevronDown />
+          <div onClick={() => setIsOpenFour(!isOpenFour)} className="title">
+            <span className="arrow">
+              {isOpenFour ? <FiChevronDown /> : <FiChevronRight />}
             </span>
             <span className="folder color-4">
               <FaFolder />
             </span>
             <p>Free Time</p>
           </div>
-
-          <div className="list">
-            <p>
-              <BsFillMarkdownFill />
-              &nbsp; reading.md
-            </p>{" "}
-            <p>
-              <BsFillMarkdownFill />
-              &nbsp; work-out.md
-            </p>{" "}
-            <p>
-              <BsFillMarkdownFill />
-              &nbsp; building-side-projects.md
-            </p>
-          </div>
+          {isOpenFour && (
+            <div className="list">
+              <p>
+                <BsFillMarkdownFill />
+                &nbsp; reading.md
+              </p>{" "}
+              <p>
+                <BsFillMarkdownFill />
+                &nbsp; work-out.md
+              </p>{" "}
+              <p>
+                <BsFillMarkdownFill />
+                &nbsp; building-side-projects.md
+              </p>
+            </div>
+          )}
         </div>
       </motion.div>
     </div>
